d18052d6c223341a41fbb6f0c20c39a6721c8c94
Guilhemmaxime <guilhemmaxime@gmail.com>
2019-12-19 08:05:55 +0100

    Update SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -35,8 +35,6 @@ class SecurityController extends AbstractFOSRestController
      */
     public function AuthenticationAction(Request $request)
     {
-        //echo 'cono2'; exit;
-        //var_dump($request->getContent()); exit;
         $data = json_decode($request->getContent(), true);
         if (empty($data['redirect-uri']) || empty($data['grant-type']) || empty($data['name'])) {
             $message = "Some datas are missing : redirect-uri or grant-type or name. Your data are:<br>" . json_encode($data);
